cf6dd8de2925943ff4ccf2f2e686d96fdaf37d78
Saikumar Laptop <saikumarlap@sdmactec.local>
2020-07-15 21:38:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function c(selector) {
+  var element = document.querySelector(selector);
+  var data = {
+    hide: () => (element.style.display = "none"),
+    on: (event, callback) => element.addEventListener(event, callback),
+    addClass: (classname) => element.classList.add(classname),
+    removeClass: (classname) => element.classList.remove(classname),
+    attr: (attr, value) => {
+      if (value) {
+        element.setAttribute(attr, value);
+      } else {
+        element.getAttribute(attr);
+      }
+    },
+    css: (obj) => {
+      for (let key in obj) {
+        element.style[key] = obj[key];
+      }
+    },
+  };
+  return data;
+}
